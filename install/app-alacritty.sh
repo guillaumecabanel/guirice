cc696868346370d1caafba84018bc229a3a15852
Guillaume CABANEL <guillaume@kuartz.fr>
2024-06-16 21:06:33 +0200

    fix rust setup

--- install/app-alacritty.sh
+++ install/app-alacritty.sh
@@ -1,5 +1,11 @@
 sudo apt install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+source $HOME/.cargo/env
+
 cargo install alacritty
+
 mkdir -p ~/.config/alacritty
 cp ~/.local/share/guirice/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
 cp ~/.local/share/guirice/configs/alacritty/theme.toml ~/.config/alacritty/theme.toml
