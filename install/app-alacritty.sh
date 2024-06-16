195a0915bc81ab00699f6eb1c7c4387169fb5bca
Guillaume CABANEL <guillaume@kuartz.fr>
2024-06-16 20:59:14 +0200

    fix install package

--- install/app-alacritty.sh
+++ install/app-alacritty.sh
@@ -1,4 +1,4 @@
-sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+sudo apt install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
 cargo install alacritty
 mkdir -p ~/.config/alacritty
 cp ~/.local/share/guirice/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
